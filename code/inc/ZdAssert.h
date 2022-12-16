930a7cc40c89bb237fd5ac037e51549be5a5ab63
mrsung82 <mrsung82@gmail.com>
2022-12-16 15:00:32 +0300

    Base fixed size types have been added. Base assert macro (without impl) added.

---
+++ code/inc/ZdAssert.h
@@ -0,0 +1,40 @@
+/*
+-----------------
+ Persistent info
+-----------------
+(C) 2012-2022 Zavod3D Design Project. Marat Sungatullin
+
+.........
+License:
+.........
+
+is in the "LICENSE" file.
+
+......
+ Web:
+......
+
+ + link_to_zdp_web_site
+
+------
+ Desc
+------
+Purpose: .
+
+----------------------
+ for developers notes
+----------------------
+
+*/
+
+#ifndef ZDP_ASSERT_H
+#define ZDP_ASSERT_H
+
+#define ZDP_ASSERT(expr) ((void)0)
+#define ZDP_ASSERT_MSG(expr, msg) ((void)0)
+#define ZDP_ASSERT_MSG1(expr, msg, a1) ((void)0)
+#define ZDP_ASSERT_MSG2(expr, msg, a1, a2) ((void)0)
+#define ZDP_ASSERT_MSG3(expr, msg, a1, a2, a3) ((void)0)
+#define ZDP_ASSERT_MSG4(expr, msg, a1, a2, a3, a4) ((void)0)
+
+#endif // ZDP_ASSERT_H
---
+++ code/inc/ZdBaseTypes.h
@@ -0,0 +1,48 @@
+/*
+-----------------
+ Persistent info
+-----------------
+(C) 2012-2022 Zavod3D Design Project. Marat Sungatullin
+
+.........
+License:
+.........
+
+is in the "LICENSE" file.
+
+......
+ Web:
+......
+
+ + link_to_zdp_website
+
+------
+ Desc
+------
+Purpose: .
+
+----------------------
+ for developers notes
+----------------------
+
+*/
+
+#ifndef ZV3D_BASETYPES_H
+#define ZV3D_BASETYPES_H
+
+
+#include <cstdint>
+
+typedef int8_t ZdInt8;
+typedef int16_t ZdInt16;
+typedef int32_t ZdInt32;
+typedef int64_t ZdInt64;
+
+typedef uint8_t ZdUInt8;
+typedef uint16_t ZdUInt16;
+typedef uint32_t ZdUInt32;
+typedef uint64_t ZdUInt64;
+
+typedef float ZdReal32;
+typedef double ZdReal64;
+#endif // ZV3D_BASETYPES_H
